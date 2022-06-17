05aeaf466447ce04b3146ee3a93ccc262f8402d3
Ethan Wisdom <ethanwisdom03@gmail.com>
2022-06-17 02:30:10 -0500

    ADDED: Main Playlist App components

--- Playlist-App/src/app/playlist/song-list/song-list.component.ts
+++ Playlist-App/src/app/playlist/song-list/song-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Song } from '../../shared/song/song.model'
 
 @Component({
   selector: 'app-song-list',
@@ -6,6 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./song-list.component.css']
 })
 export class SongListComponent implements OnInit {
+  mySongs: Song[] = [
+    new Song(
+      'Happy Birthday',
+      'Ethan Wisdom',
+      'Traditonal Folk',
+      'https://unsplash.com/photos/18N4okmWccM'
+
+    )
+  ];
 
   constructor() { }
 
---
+++ Playlist-App/src/app/shared/song/song.model.ts
@@ -0,0 +1,8 @@
+export class Song {
+  constructor(
+    public title: string,
+    public artist: string,
+    public genre: string,
+    public albumCoverPath: string
+  ) {}
+}
